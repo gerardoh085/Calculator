2fddd51c6efa791c873e40138bb177e2da0001e3
gerardoh085 <gerardoh085@gmail.com>
2023-07-03 16:30:13 -0400

    deleted console.logs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var state = {
     fnum: '',
     rnum: 0,
-    pnum: '',
+    pnum: ''
 }
 const ready = ()=>{
     
@@ -25,7 +25,6 @@ const ready = ()=>{
             inpClass.value += event.target.innerText;
             inpClass.scrollLeft = inpClass.scrollWidth
             
-            console.log(event.target.innerText);
             
             state.fnum += event.target.innerText;
             
@@ -47,19 +46,12 @@ const ready = ()=>{
             state.pnum = state.fnum;
             state.fnum = '';
             inpClass.value = state.fnum
-            console.log("fnum + : " + state.fnum);
-            console.log("pnum + : " + state.pnum);
         }else if(state.pnum !== '' && state.fnum !== ''){
-            // in case user presses '+' again:
-            // eval the pnum and fnum:
-            // store result in pnum
-            console.log("ENTERED")
+            
             state.pnum = parseFloat(state.pnum) + parseFloat(state.fnum);
             state.fnum = '';
             inpClass.value = state.pnum;
             
-            // state.res = parseFloat(state.pnum);
-            console.log("result: "+state.pnum);
         }
     });
     // subtraction
@@ -70,15 +62,11 @@ const ready = ()=>{
             state.pnum = state.fnum;
             state.fnum = '';
             inpClass.value = state.fnum
-            console.log("fnum + : " + state.fnum);
-            console.log("pnum + : " + state.pnum);
         }else if(state.pnum !== '' && state.fnum !== ''){
 
             state.pnum = parseFloat(state.pnum) - parseFloat(state.fnum);
             state.fnum = '';
             inpClass.value = state.pnum;
-
-            console.log("result: "+state.pnum);
         }
     });
     //multiplication:
@@ -89,15 +77,11 @@ const ready = ()=>{
             state.pnum = state.fnum;
             state.fnum = '';
             inpClass.value = state.fnum
-            console.log("fnum + : " + state.fnum);
-            console.log("pnum + : " + state.pnum);
         }else if(state.pnum !== '' && state.fnum !== ''){
 
             state.pnum = parseFloat(state.pnum) * parseFloat(state.fnum);
             state.fnum = '';
             inpClass.value = state.pnum;
-
-            console.log("result: "+state.pnum);
         }
     });
     //division
@@ -108,15 +92,12 @@ const ready = ()=>{
             state.pnum = state.fnum;
             state.fnum = '';
             inpClass.value = state.fnum
-            console.log("fnum + : " + state.fnum);
-            console.log("pnum + : " + state.pnum);
         }else if(state.pnum !== '' && state.fnum !== ''){
 
             state.pnum = parseFloat(state.pnum) / parseFloat(state.fnum);
             state.fnum = '';
             inpClass.value = state.pnum;
 
-            console.log("result: "+state.pnum);
         }
     });
 
@@ -124,12 +105,8 @@ const ready = ()=>{
 
     var eqls = document.getElementsByClassName("equal")[0];
     eqls.addEventListener("click",()=>{
-        // if state is '' then do nothing 
-        // if both pnum and fnum are not '' then compute
-        // if fnum = '' then do nothing
         
         if(state.fnum !== '' && state.pnum !== ''){
-            console.log(op);
             if(op === '+'){
                 state.res = parseFloat(state.pnum) + parseFloat(state.fnum);
             }else 
@@ -157,10 +134,10 @@ const delBtn = (inpClass,state)=>{
     
     var del = document.getElementsByClassName("del")[0];
     del.addEventListener("click",(event)=>{
-        console.log("inside");
+        
         var text = state.fnum;
         if(state.fnum.length > 0){
-            console.log("inside 2");
+            
             state.fnum = state.fnum.slice(0,-1); //removes the last character
             inpClass.value = state.fnum;
         }
